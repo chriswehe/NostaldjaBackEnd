3d5d4a01170726bbba243bcd89e59d4b92684c44
CHRISTOPHER_WEHE <chriswehe@gmail.com>
2019-10-15 11:07:21 -0400

    Removed hyperlinked serialization

--- nostaldja/serializers.py
+++ nostaldja/serializers.py
@@ -2,19 +2,15 @@ from rest_framework import serializers
 
 from .models import Decade, Fad
 
-class DecadeSerializer(serializers.ModelSerializer):
-    fads = serializers.HyperlinkedRelatedField(
-        view_name='fad_detail',
-        many=True,
-        read_only=True
-    )
-
-    class Meta:
-        model = Decade
-        fields = ('start_year', 'fads',)
-
-
 class FadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fad
         fields = ('name', 'image_url', 'description', 'decade',)
+
+class DecadeSerializer(serializers.ModelSerializer):
+
+    fads = FadSerializer(many=True)
+
+    class Meta:
+        model = Decade
+        fields = ('start_year', 'fads',)
